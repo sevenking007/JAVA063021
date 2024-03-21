71d769dd34e1b13ebb79cf02a2d6352fa648a943
ruxia <ruxiaolai@qq.com>
2024-03-21 18:42:08 +0800

    测试

---
+++ test01/src/Test.java
@@ -0,0 +1,12 @@
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ * User:ruxia
+ * Date:2024-03-21
+ * Time:{TIME}
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("heelo");
+    }
+}
