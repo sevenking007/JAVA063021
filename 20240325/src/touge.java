cd390dad06b569ab3d7bf74e51a80104ca45c0fb
yangyang007 <129572862+sevenking007@users.noreply.github.com>
2024-03-25 21:27:43 +0800

    练习

---
+++ 20240325/src/touge.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ * User:ruxia
+ * Date:2024-03-25
+ * Time:{TIME}
+ */
+public class touge {
+    public static void main(String[] args) {
+      /*  Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        *//*********start*********//*
+        System.out.println("a==b="+(a==b));
+        System.out.println("a!=b="+(a!=b));
+        System.out.println("a>b="+(a>b));
+        System.out.println("a<b="+(a<b));
+        System.out.println("b>=a="+(b>=a));
+        System.out.println("b<=a="+(b<=a));*/
+        Scanner input = new Scanner(System.in);
+/*        *//******start******//*
+        System.out.println("请输入学员成绩：");
+        int score=input.nextInt();
+        if(score>85&&score<100){
+            System.out.println("优，非常棒！");
+        }else{
+            System.out.print("良，下次加油！");
+        }*/
+
+
+
+    }
+}
